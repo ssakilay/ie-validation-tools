303888c0d267eda359f1aaa95927aa06fba49bff
fmeng <fmeng@mii.ucla.edu>
2019-01-03 18:22:16 -0800

    added lib folder to classpath

--- run/create-db-tables.sh
+++ run/create-db-tables.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xmx8g -cp ie-validation-tools.jar ietools.CreateDBTables $1 $2 $3 $4 $5 $6
+java -Xmx8g -cp ie-validation-tools.jar:./lib/* ietools.CreateDBTables $1 $2 $3 $4 $5 $6
